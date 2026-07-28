1a262bf6fd8fbdcdda60a9e691e71ec15ad48001
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:25:01 +0530

    Update __main__.py

--- Chizuru/__main__.py
+++ Chizuru/__main__.py
@@ -1,1 +1,21 @@
-.
+import asyncio
+import importlib
+from pyrogram import idle
+from Chizuru.modules import ALL_MODULES
+from Chizuru import Chizuru, userbot, pytgcalls
+
+loop = asyncio.get_event_loop()
+
+async def start():
+    await Chizuru.start()
+    await userbot.start()
+    await pytgcalls.start()
+    
+    for module in ALL_MODULES:
+        importlib.import_module(f"Chizuru.modules.{module}")
+    
+    print("✅ Bot Started Successfully!")
+    await idle()
+
+if __name__ == "__main__":
+    loop.run_until_complete(start())
