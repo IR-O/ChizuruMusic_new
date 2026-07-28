04c5293c80764ce7d3bcf1afdc6a713eac877790
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 23:05:53 +0530

    Update __init__.py

--- Chizuru/__init__.py
+++ Chizuru/__init__.py
@@ -4,47 +4,87 @@ import logging
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+from config import (
+    API_ID,
+    API_HASH,
+    BOT_TOKEN,
+    SESSION_STRING
+)
 
 
 logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    format="[%(levelname)s] %(asctime)s - %(name)s: %(message)s",
     level=logging.INFO
 )
 
 
+# Telegram Bot
+
 Chizuru = Client(
-    "Chizuru",
+    "ChizuruBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
 
+# User Account (Assistant)
+
 userbot = Client(
-    "userbot",
+    "ChizuruAssistant",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=SESSION_STRING
 )
 
 
-pytgcalls = PyTgCalls(userbot)
+# PyTgCalls
+
+pytgcalls = PyTgCalls(
+    userbot
+)
 
 
-async def chizuru_music():
+BOT_ID = None
+BOT_NAME = None
+BOT_USERNAME = None
+
+
+
+async def start():
+
+    global BOT_ID
+    global BOT_NAME
+    global BOT_USERNAME
+
 
     await Chizuru.start()
 
     await userbot.start()
+
 
     await pytgcalls.start()
 
 
     me = await Chizuru.get_me()
 
+
+    BOT_ID = me.id
+
+    BOT_USERNAME = me.username
+
+    BOT_NAME = (
+        me.first_name
+        + (
+            f" {me.last_name}"
+            if me.last_name
+            else ""
+        )
+    )
+
+
     logging.info(
-        f"Bot Started: @{me.username}"
+        f"Bot Started: @{BOT_USERNAME}"
     )
 
 
@@ -52,7 +92,18 @@ async def chizuru_music():
 
 
 
+async def stop():
+
+    await pytgcalls.stop()
+
+    await userbot.stop()
+
+    await Chizuru.stop()
+
+
+
 if __name__ == "__main__":
+
     asyncio.get_event_loop().run_until_complete(
-        chizuru_music()
+        start()
     )
