4d2d98171f547cd972b584a7dcbf2d9284ad07cc
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:29:13 +0530

    Update usersdb.py

--- Chizuru/core/mongo/usersdb.py
+++ Chizuru/core/mongo/usersdb.py
@@ -1,1 +1,15 @@
-.
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from config import MONGO_URL
+
+mongo = MongoCli(MONGO_URL)
+db = mongo.usersdb
+
+async def get_users():
+    return [user['user'] async for user in db.users.find()]
+
+async def add_user(user):
+    if not await db.users.find_one({"user": user}):
+        await db.users.insert_one({"user": user})
+
+async def del_user(user):
+    await db.users.delete_one({"user": user})
