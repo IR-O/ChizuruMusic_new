f791275c96aa4c9775764f77951ded2563e3e192
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:34:16 +0530

    Update stats.py

--- Chizuru/modules/stats.py
+++ Chizuru/modules/stats.py
@@ -1,1 +1,10 @@
-.
+from pyrogram import filters
+from Chizuru import Chizuru
+from config import OWNER_ID
+from Chizuru.core.mongo import get_users, get_chats
+
+@Chizuru.on_message(filters.command(["stats"], prefixes=["/", "."]) & filters.user(OWNER_ID))
+async def stats(_, message):
+    users = len(await get_users())
+    chats = len(await get_chats())
+    await message.reply_text(f"📊 **Bot Stats**\n\n👤 Users: {users}\n💬 Chats: {chats}")
