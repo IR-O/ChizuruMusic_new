5d928530538159eeca69de450c44344b6532fbba
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:29:46 +0530

    Update __init__.py

--- Chizuru/modules/__init__.py
+++ Chizuru/modules/__init__.py
@@ -1,1 +1,9 @@
-.
+import glob
+from os.path import basename, dirname, isfile
+
+def __list_all_modules():
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    return [basename(f)[:-3] for f in mod_paths if isfile(f) and not f.endswith("__init__.py")]
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
