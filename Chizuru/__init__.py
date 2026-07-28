b9f380c7d6639baf6196579d49e8536de614f594
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:24:13 +0530

    Update __init__.py

--- Chizuru/__init__.py
+++ Chizuru/__init__.py
@@ -1,1 +1,26 @@
-.
+import asyncio
+import logging
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.INFO
+)
+
+Chizuru = Client(
+    ":Chizuru:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+userbot = Client(
+    ":userbot:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION_STRING,
+)
+
+pytgcalls = PyTgCalls(userbot)
