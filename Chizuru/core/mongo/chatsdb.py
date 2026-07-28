c119778456ffada155744012f4dc8fe5f3644773
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-29 00:27:34 +0530

    Add files via upload

--- Chizuru/core/mongo/chatsdb.py
+++ Chizuru/core/mongo/chatsdb.py
@@ -1,15 +1,39 @@
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_URL
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 mongo = MongoCli(MONGO_URL)
-db = mongo.chatsdb
+db = mongo.chats
+db = db.chatsdb
+
 
 async def get_chats():
-    return [chat['chat'] async for chat in db.chats.find()]
+  chat_list = []
+  async for chat in db.chats.find({"chat": {"$lt": 0}}):
+    chat_list.append(chat['chat'])
+  return chat_list
+
+async def get_chat(chat):
+  chats = await get_chats()
+  if chat in chats:
+    return True
+  else:
+    return False
 
 async def add_chat(chat):
-    if not await db.chats.find_one({"chat": chat}):
-        await db.chats.insert_one({"chat": chat})
+  chats = await get_chats()
+  if chat in chats:
+    return
+  else:
+    await db.chats.insert_one({"chat": chat})
 
 async def del_chat(chat):
+  chats = await get_chats()
+  if not chat in chats:
+    return
+  else:
     await db.chats.delete_one({"chat": chat})
+
+
+
+
+
