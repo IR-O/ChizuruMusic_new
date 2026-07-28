e68878a4903d549480a32e765e0ddb3e2fae7b1f
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:30:50 +0530

    Update ping.py

--- Chizuru/modules/ping.py
+++ Chizuru/modules/ping.py
@@ -1,1 +1,10 @@
-.
+import time
+from pyrogram import filters
+from Chizuru import Chizuru
+
+@Chizuru.on_message(filters.command(["ping"], prefixes=["/", "."]))
+async def ping(_, message):
+    start = time.time()
+    msg = await message.reply_text("Pinging...")
+    end = time.time()
+    await msg.edit_text(f"🏓 **Pong!**\n`{round((end-start)*1000, 2)} ms`")
