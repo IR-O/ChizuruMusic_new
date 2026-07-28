3b2d2017300bed227884c34f1f6a6a4a2da12792
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:28:42 +0530

    Update chatsdb.py

--- Chizuru/core/mongo/chatsdb.py
+++ Chizuru/core/mongo/chatsdb.py
@@ -1,1 +1,15 @@
-.
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from config import MONGO_URL
+
+mongo = MongoCli(MONGO_URL)
+db = mongo.chatsdb
+
+async def get_chats():
+    return [chat['chat'] async for chat in db.chats.find()]
+
+async def add_chat(chat):
+    if not await db.chats.find_one({"chat": chat}):
+        await db.chats.insert_one({"chat": chat})
+
+async def del_chat(chat):
+    await db.chats.delete_one({"chat": chat})
