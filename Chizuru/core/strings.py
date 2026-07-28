64a27863bb580825b3c6c7a52237ceed2efcd4dd
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:25:55 +0530

    Update strings.py

--- Chizuru/core/strings.py
+++ Chizuru/core/strings.py
@@ -1,1 +1,25 @@
-strings.py
+start_txt = """
+**ʜᴇʟʟᴏ** {} 
+
+**ɪ ᴀᴍ ᴄʜɪᴢᴜʀᴜ, ʏᴏᴜʀ ᴍᴜsɪᴄ ᴠɪʀᴛᴜᴏsᴏ!**
+"""
+
+help_txt = """
+**» ᴄʜɪᴢᴜʀᴜ ᴄᴏᴍᴍᴀɴᴅs**
+- /play - Play music
+- /skip - Skip song
+- /pause - Pause music
+- /resume - Resume music
+- /end - Stop music
+- /volume [1-200] - Change volume
+"""
+
+music_txt = "**» ᴍᴜsɪᴄ ᴄᴏᴍᴍᴀɴᴅs**\n/play - Play song\n/vplay - Play video\n/skip - Skip song"
+ai_txt = "**» ᴀɪ ᴄᴏᴍᴍᴀɴᴅs**\n/gpt - Chat with GPT\n/bard - Chat with Bard"
+bass_txt = "**» ʙᴀss ᴇғғᴇᴄᴛs**\n/bass - Add bass boost\n/loudly - Make louder\n/mono - Convert to mono"
+youtube_txt = "**» ʏᴏᴜᴛᴜʙᴇ ᴄᴏᴍᴍᴀɴᴅs**\n/song - Download audio\n/video - Download video"
+misc_txt = "**» ᴍɪsᴄ ᴄᴏᴍᴍᴀɴᴅs**\n/ping - Check latency\n/stats - Bot stats"
+broadcast_txt = "**» ʙʀᴏᴀᴅᴄᴀsᴛ**\n/broadcast - Send message to all users"
+checker_txt = "**» ᴄʜᴇᴄᴋᴇʀ**\n/check - Check bot status"
+devs_txt = "**» ᴅᴇᴠᴇʟᴏᴘᴇʀ**\nOwner: @itz_rehan_786"
+instagram_txt = "**» ɪɴsᴛᴀɢʀᴀᴍ**\n/insta - Instagram downloader"
