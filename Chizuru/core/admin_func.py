a4eff7b8ec529c917cc503533b87da62914c0572
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-28 20:26:25 +0530

    Update admin_func.py

--- Chizuru/core/admin_func.py
+++ Chizuru/core/admin_func.py
@@ -1,1 +1,19 @@
-.
+from config import SUDO_USERS
+from typing import List, Dict, Callable
+
+admins: Dict[int, List[int]] = {}
+
+def set_admins(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+def get_admins(chat_id: int):
+    return admins.get(chat_id, [])
+
+def authorized_users(func: Callable) -> Callable:
+    async def decorator(client, message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+        admins_list = get_admins(message.chat.id)
+        if message.from_user.id in admins_list:
+            return await func(client, message)
+    return decorator
