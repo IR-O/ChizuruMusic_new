e17b25bfe7ecd76d5b70d6cadbc0231329c96a90
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-29 09:01:12 +0530

    Update __init__.py

--- Chizuru/__init__.py
+++ Chizuru/__init__.py
@@ -1,28 +1,32 @@
 import asyncio
 import logging
-from pytgcalls import PyTgCalls
+
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+from pytgcalls import PyTgCalls
 
-
-loop = asyncio.get_event_loop()
+from config import (
+    API_ID,
+    API_HASH,
+    BOT_TOKEN,
+    SESSION_STRING,
+)
 
 logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    format="[%(levelname)s %(asctime)s] %(name)s: %(message)s",
     level=logging.INFO,
 )
 
-
+loop = asyncio.get_event_loop()
 
 Chizuru = Client(
-    ":Chizuru:",
+    "ChizuruBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
 )
 
 userbot = Client(
-    ":userbot:",
+    "ChizuruAssistant",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=SESSION_STRING,
@@ -30,22 +34,36 @@ userbot = Client(
 
 pytgcalls = PyTgCalls(userbot)
 
+BOT_ID = None
+BOT_NAME = None
+BOT_USERNAME = None
 
 
 async def chizuru_music():
     global BOT_ID, BOT_NAME, BOT_USERNAME
+
     await Chizuru.start()
+    logging.info("Bot Client Started.")
+
     await userbot.start()
+    logging.info("Assistant Client Started.")
+
     await pytgcalls.start()
-    getme = await Chizuru.get_me()
-    BOT_ID = getme.id
-    BOT_USERNAME = getme.username
-    if getme.last_name:
-        BOT_NAME = getme.first_name + " " + getme.last_name
-    else:
-        BOT_NAME = getme.first_name
+    logging.info("PyTgCalls Started.")
+
+    me = await Chizuru.get_me()
+
+    BOT_ID = me.id
+    BOT_USERNAME = me.username
+    BOT_NAME = (
+        f"{me.first_name} {me.last_name}"
+        if me.last_name
+        else me.first_name
+    )
+
+    logging.info(f"Bot Name: {BOT_NAME}")
+    logging.info(f"Bot Username: @{BOT_USERNAME}")
+    logging.info(f"Bot ID: {BOT_ID}")
 
 
 loop.run_until_complete(chizuru_music())
-
-
