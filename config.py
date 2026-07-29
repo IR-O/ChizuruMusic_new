13e6d2136c32727b3ca9a9bfee78ecaf7d22e72e
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-29 13:06:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,29 @@
-from os import getenv
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-MONGO_URL = getenv("MONGO_URL", "")
-SESSION_STRING = getenv("SESSION_STRING", "")
+class Config:
+    # Bot Account
+    API_ID: int = int(os.environ.get("API_ID", 24208695))
+    API_HASH: str = os.environ.get("API_HASH", "fa96a7eb2dffe7f4cc8ba1399b68d24d")
+    BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "6365132039:AAF48I0KgZe4cyHmhMiRx_K634u6BEKApDQ")
+    
+    # Assistant Account
+    ASSISTANT_SESSION: str = os.environ.get("ASSISTANT_SESSION", "BQFxZTcAS6lmQ586CKMgSTQtRPUBonBJoTku2NN0vecIwGtqmz4N2bls5T-F37bWuMWEkexHvNtZF0XhodZsdiC6AOmD0CNm27zFkr1M8lCm-hzoGVlZ30aAgSu786py_6brN-lc6zmnflTu7am0Kx26Nl5YwP0slTZBaA9rHnaRy4nh3BgImP2we6SHej6PoqI6o22eyguy0XdsE9q1Jw7RhCF7egNk7fwd1npi0C5FuRJa9ArmnTsfwrWoWYp79BbkC6bkUGbNJ5kO0eTdRUbnZUkl4AxsdeiH7woS_DayoNUrpYjEjPNLRPsloXKjzcV0A47S1Ue0Q9vPDqfqCrq45ef_1wAAAAFI8Y5zAA")
+    ASSISTANT_API_ID: int = int(os.environ.get("ASSISTANT_API_ID", 24208695))
+    ASSISTANT_API_HASH: str = os.environ.get("ASSISTANT_API_HASH", "fa96a7eb2dffe7f4cc8ba1399b68d24d")
+    
+    # Owner & Logging
+    OWNER_ID: int = int(os.environ.get("OWNER_ID", 8769597773))
+    LOGGER_ID: int = int(os.environ.get("LOGGER_ID", -1001234567890))
+    
+    # MongoDB
+    MONGO_URL: str = os.environ.get("MONGO_URL", "")
+    
+    # Bot Settings
+    DURATION_LIMIT: int = 300
+    SESSION_NAME: str = "ChizuruMusic"
+    
+    # Strings
+    START_IMG_URL: str = "https://graph.org/file/e3fa9ab16ebefbfdd29d9.jpg"
